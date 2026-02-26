fcb1b3f9aed10eb286facc5b93529de509d03e8a
TanishqJangir <tanishqjangir001@gmail.com>
2026-02-26 09:48:41 +0530

    Empty execution page done

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -39,6 +39,8 @@ export const executeWorkflow = inngest.createFunction(
   },
   async ({ event, step, publish }) => {
 
+    const inngestEventId = event.id;
+    
     const workflowId = event.data.workflowId;
 
     if (!workflowId) {
